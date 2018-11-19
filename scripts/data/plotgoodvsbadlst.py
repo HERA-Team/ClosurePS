8833534b840aa359c86e059c3c0469e1e5a7572b
James Kent <jck42@cam.ac.uk>
2018-11-19 14:47:22 +0000

    Plots circular standard deviation for mulitple days in dataset

--- scripts/data/plotgoodvsbadlst.py
+++ scripts/data/plotgoodvsbadlst.py
@@ -13,6 +13,7 @@ def main():
     command = argparse.ArgumentParser()
     command.add_argument('filepath', help = ('.npz directory'))
     command.add_argument('working_directory', help = ('Working Directory. Where files will be put.'))
+    command.add_argument('--multiple_days', action='store_true',required=False, help=('Will parse a directory of closures from different days and sort through them'))
     args = command.parse_args()
 
     if (os.path.exists(args.working_directory) == False):
@@ -25,83 +26,140 @@ def main():
             npz_files.append(args.filepath+file)
 
 
-    # Concatenate all the .npz files:
-    all_closures = None
-    all_last = None
-    for file in npz_files:
-        with numpy.load(file) as data:
-            closures = data['phase']
-            last = data['LAST']
-            all_closures = numpy.zeros(shape=numpy.shape(closures))
-            all_last = numpy.zeros(shape=numpy.shape(last))
-        break
-    print(numpy.shape(all_closures))
-    print(numpy.shape(all_last))
-    for file in sorted(npz_files):
-        print(file)
-        with numpy.load(file) as data:
-            closures = data['phase']
-            flags = data['flags']
-            last = data['LAST']
-            print(numpy.shape(closures))
-            all_closures = numpy.concatenate((all_closures,closures),axis=3)
-            all_last = numpy.concatenate((all_last, last))
+    if args.multiple_days:
+        detected_datestamps = []
+        for file in npz_files:
+            detected_datestamps.append(file.split('.')[1])
+        detected_datestamps = set(detected_datestamps)
+        detected_datestamps = sorted(detected_datestamps)
+
+        
+        #First find all files with a certain datestamp
+        for datestamp in detected_datestamps:
+            all_closures = None
+            all_last = None
+            for file in npz_files:
+                if file.split('.')[1] == datestamp:
+                    with numpy.load(file) as data:
+                        closures = data['phase']
+                        last=data['LAST']
+                        all_closures = numpy.zeros(shape=numpy.shape(closures))
+                        all_last = numpy.zeros(shape=numpy.shape(last))
+                    break
+            for file in sorted(npz_files):
+                if file.split('.')[1] == datestamp:
+                    with numpy.load(file) as data:
+                        closures = data['phase']
+                        flags = data['flags']
+                        last = data['LAST']
+                        all_closures=numpy.concatenate((all_closures,closures),axis=3)
+                        all_last=numpy.concatenate((all_last,last))
+            all_closures = all_closures[:,:,:,60:]
+            all_last = all_last[60:]
+            all_closures_x = numpy.cos(all_closures)
+            all_closures_y = numpy.sin(all_closures)
+            averaged_x = numpy.mean(all_closures_x, axis=0)
+            averaged_y = numpy.mean(all_closures_y, axis=0)
+            r = numpy.sqrt(numpy.square(averaged_x) + numpy.square(averaged_y))
+            std_dev = numpy.sqrt(-2 * numpy.log(r))
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_title("LST Quality on JD "+datestamp)
 
 
 
-    # Find standard deviation across triads at every timestamp and channel:
+            plt.imshow(std_dev[0,:,:])
 
-    all_closures = all_closures[:,:,:,60:]
-    all_last = all_last[60:]
-    all_closures_x = numpy.cos(all_closures)
-    all_closures_y = numpy.sin(all_closures)
+            xticks = ["{:2.3f}".format((lst %1 ) * 24) for lst in all_last]
+            ax.set_xticks(numpy.arange(0,numpy.shape(all_last)[0]))
+            ax.set_xticklabels(xticks)
+            for label in ax.xaxis.get_ticklabels():
+                label.set_visible(False)
+            for label in ax.xaxis.get_ticklabels()[::500]:
+                label.set_visible(True)    
+                    #ax.set_xticklabels(["{:6.4f}".format(i) for i in all_last])
+            plt.ylabel("Channel")
+            plt.xlabel("LST")
+            # ax.set_xlabel((all_last[0],all_last[-1]))
+            plt.clim(0,2.5)
+            plt.colorbar(orientation='horizontal')
+            plt.savefig(args.working_directory+datestamp+".eps")
+
+            
+
+    # Case of a directory with just one file in it, or concatenate EVERY day together. Why would you do this?
+    else:
+            
+        # Concatenate all the .npz files:
+        all_closures = None
+        all_last = None
+        for file in npz_files:
+            with numpy.load(file) as data:
+                closures = data['phase']
+                last = data['LAST']
+                all_closures = numpy.zeros(shape=numpy.shape(closures))
+                all_last = numpy.zeros(shape=numpy.shape(last))
+            break
+        print(numpy.shape(all_closures))
+        print(numpy.shape(all_last))
+        for file in sorted(npz_files):
+            print(file)
+            with numpy.load(file) as data:
+                closures = data['phase']
+                flags = data['flags']
+                last = data['LAST']
+                print(numpy.shape(closures))
+                all_closures = numpy.concatenate((all_closures,closures),axis=3)
+                all_last = numpy.concatenate((all_last, last))
+
+        # Find standard deviation across triads at every timestamp and channel:
+
+        all_closures = all_closures[:,:,:,60:]
+        all_last = all_last[60:]
+        all_closures_x = numpy.cos(all_closures)
+        all_closures_y = numpy.sin(all_closures)
     
-    averaged_x = numpy.mean(all_closures_x,axis=0)
-    averaged_y = numpy.mean(all_closures_y,axis=0)
-    r = numpy.sqrt(numpy.square(averaged_x) + numpy.square(averaged_y))
-    std_dev = numpy.sqrt(-2 * numpy.log(r))
+        averaged_x = numpy.mean(all_closures_x,axis=0)
+        averaged_y = numpy.mean(all_closures_y,axis=0)
+        r = numpy.sqrt(numpy.square(averaged_x) + numpy.square(averaged_y))
+        std_dev = numpy.sqrt(-2 * numpy.log(r))
 
 
     
-    print(numpy.argwhere(numpy.logical_and(all_last < 64834.1067, all_last > 64834.08)))
-    print(numpy.shape(numpy.logical_and(all_last < 64834.1067, all_last > 64834.08)))
+        print(numpy.argwhere(numpy.logical_and(all_last < 64834.1067, all_last > 64834.08)))
+        print(numpy.shape(numpy.logical_and(all_last < 64834.1067, all_last > 64834.08)))
     
-    print("All Closures/Std_dev Shape:")
-    print(numpy.shape(all_closures))
-    print(numpy.shape(std_dev))
-    print("----------------------------")
-
-    
+        print("All Closures/Std_dev Shape:")
+        print(numpy.shape(all_closures))
+        print(numpy.shape(std_dev))
+        print("----------------------------")
 
     
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title("LST Quality on JD 2458102")
 
-
-
-    plt.imshow(std_dev[0,:,:])
-
-#    ticks = numpy.linspace(0,numpy.shape(all_closures)[1])
-    #last_subset = all_last[1::500]
-    #print(last_subset)
     
-    xticks = ["{:2.3f}".format((lst %1 ) * 24) for lst in all_last]
-    ax.set_xticks(numpy.arange(0,numpy.shape(all_last)[0]))
-    ax.set_xticklabels(xticks)
-    for label in ax.xaxis.get_ticklabels():
-        label.set_visible(False)
-    for label in ax.xaxis.get_ticklabels()[::500]:
-        label.set_visible(True)    
-    #ax.set_xticklabels(["{:6.4f}".format(i) for i in all_last])
-    plt.ylabel("Channel")
-    plt.xlabel("LST")
-    # ax.set_xlabel((all_last[0],all_last[-1]))
-    plt.clim(0,2.5)
-    plt.colorbar(orientation='horizontal')
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.set_title("LST Quality on JD 2458160")
 
-    numpy.savez("std_dev_lst",sigma=std_dev[0,:,:],lst=numpy.array(xticks))
-    plt.show()
+
+
+        plt.imshow(std_dev[0,:,:])
+
+        xticks = ["{:2.3f}".format((lst %1 ) * 24) for lst in all_last]
+        ax.set_xticks(numpy.arange(0,numpy.shape(all_last)[0]))
+        ax.set_xticklabels(xticks)
+        for label in ax.xaxis.get_ticklabels():
+            label.set_visible(False)
+        for label in ax.xaxis.get_ticklabels()[::500]:
+            label.set_visible(True)    
+        #ax.set_xticklabels(["{:6.4f}".format(i) for i in all_last])
+        plt.ylabel("Channel")
+        plt.xlabel("LST")
+        # ax.set_xlabel((all_last[0],all_last[-1]))
+        plt.clim(0,2.5)
+        plt.colorbar(orientation='horizontal')
+        plt.show()
 
         
 if __name__=="__main__":
