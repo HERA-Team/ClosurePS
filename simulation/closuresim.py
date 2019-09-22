89f7ca58f1737970a4b5d954128694645e76b0ee
James Kent <jameschristopherkent@gmail.com>
2019-09-22 09:53:20 +0100

    Up to date current work on modelling

--- simulation/closuresim.py
+++ simulation/closuresim.py
@@ -1,6 +1,7 @@
 import numpy
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D 
+from scipy.special import jv
 
 # For co-ordinate transforms
 
@@ -102,10 +103,11 @@ def find_beam_at_point(beam,point):
     
     # Multiple real/imaginary seperately. 
     
-    re_be = beam[bl_ind].real * beam[bm_ind].real
-    im_be = beam[bl_ind].imag * beam[bm_ind].imag    
+    be = beam[bl_ind,0] * beam[bm_ind,0]
+
     
-    return re_be + 1j * im_be
+    return be
+
 
     
 
@@ -232,6 +234,39 @@ def generate_gaussian_power_beams(locs,mean,lb,ub,lbp,ubp):
     
     
     assert(locs.shape[0] == mean.shape[0]) #== sigmas.shape[0])
+    beams_matrix = numpy.zeros(shape=(locs.shape[0],2000,2),dtype=numpy.float64)
+    x = numpy.linspace(-1,1,2000)
+    
+    for i in numpy.arange(locs.shape[0]):
+        sigma = numpy.random.uniform(lb,ub)
+        sigma_imag = numpy.random.uniform(lbp,ubp)
+        beams_matrix[i,:,0] = numpy.exp(-(((x-mean[i,0])**2)/(2*sigma**2)))
+        beams_matrix[i,:,1] = numpy.exp(-(((x-mean[i,0])**2)/(2*sigma**2)))
+        
+        # Normalise to unit amplitude
+        beam_peak = numpy.max(numpy.abs(beams_matrix[i,:]))
+        beams_matrix[i,:] /= beam_peak
+        
+    mean_beam = numpy.mean(beams_matrix,axis=0) # Beam error
+    beam_error = numpy.abs(mean_beam - beams_matrix)
+    av_beam_error = numpy.mean(beam_error)
+    print(av_beam_error)
+    
+
+    
+    return x,beams_matrix,av_beam_error
+
+def generate_gaussian_power_beams_wi(locs,mean,lb,ub,lbp,ubp):
+    
+    # Locs = antenna locs in u/v/w (wavelengths)
+    # Mean = Mean loc of antenna beam. Zero will suffice if centred on zenith in direction cosine co-ordinates
+    # lb = Lower bound of sigmas for real part of beam
+    # ub = Upper bound ""
+    # lbp = Lower bound of sigmas for imaginary party of beam
+    # ubp = Upper bound "" 
+    
+    
+    assert(locs.shape[0] == mean.shape[0]) #== sigmas.shape[0])
     beams_matrix = numpy.zeros(shape=(locs.shape[0],2000),dtype=numpy.complex128)
     x = numpy.linspace(-1,1,2000)
     
@@ -252,6 +287,53 @@ def generate_gaussian_power_beams(locs,mean,lb,ub,lbp,ubp):
 
     
     return x,beams_matrix,av_beam_error
+
+
+def generate_gaussian_power_beams_with_sidelobes(locs,mean,lb,ub,lbp,ubp):
+    
+    # Locs = antenna locs in u/v/w (wavelengths)
+    # Mean = Mean loc of antenna beam. Zero will suffice if centred on zenith in direction cosine co-ordinates
+    # lb = Lower bound of sigmas for real part of beam
+    # ub = Upper bound ""
+    # lbp = Lower bound of sigmas for imaginary party of beam
+    # ubp = Upper bound "" 
+    
+    
+    assert(locs.shape[0] == mean.shape[0]) #== sigmas.shape[0])
+    beams_matrix = numpy.zeros(shape=(locs.shape[0],2000),dtype=numpy.complex128)
+    x = numpy.linspace(-1,1,2000)
+    
+    for i in numpy.arange(locs.shape[0]):
+        sigma = numpy.random.uniform(lb,ub)
+        sigma_imag = numpy.random.uniform(lbp,ubp)
+        beams_matrix[i,:] = numpy.exp(-(((x-mean[i])**2)/(2*sigma**2)))# + 1j*numpy.exp(-(((x-mean[i])**2)/(2*sigma_imag**2)))
+        
+        # Normalise to unit amplitude
+        beam_peak = numpy.max(numpy.abs(beams_matrix[i,:]))
+        beams_matrix[i,:] /= beam_peak
+        
+    mean_beam = numpy.mean(beams_matrix,axis=0) # Beam error
+    beam_error = numpy.abs(mean_beam - beams_matrix)
+    av_beam_error = numpy.mean(beam_error)
+    print(av_beam_error)
+    
+
+    
+    return x,beams_matrix,av_beam_error
+
+
+def generate_airy_disk(sf):
+    
+    # Generates airy disc function in sky cosine co-ordinates
+    
+    ls = numpy.linspace(-1,1,2000)
+    ms = numpy.linspace(-1,1,2000)
+    
+    LS,MS = numpy.meshgrid(ls,ms)
+    
+    airy = (2*jv(1,sf*(LS**2 + MS **2))/(sf*(LS**2 + MS**2)))**2
+    
+    return airy
     
 def simulate_visibilities(layout,points,beam):
     
@@ -376,7 +458,39 @@ def simulate_visibilities_beams_sky(layout,sky,beams):
                     point_amp = sky[mc,lc]
                     vis_matrix[i,j] += point_amp * bap * phase_factor
                 
-    return vis_matrix/(grid_size**2)   
+    return vis_matrix/(grid_size**2)  
+
+
+def simulate_visibilities_airy_sky(layout,sky,beams):
+    
+    vis_matrix = numpy.zeros(shape=(layout.shape[0],layout.shape[0]),dtype=numpy.complex128)
+    grid_size = sky.shape[0]
+    lm_step = 2/grid_size
+    
+    for i in numpy.arange(layout.shape[0]):
+        for j in numpy.arange(layout.shape[0]):        
+            
+            ud = layout[i,0] - layout[j,0]
+            vd = layout[i,1] - layout[j,1]
+            wd = layout[i,2] - layout[j,2]
+            lm_step = 2/grid_size       
+            
+            for lc in numpy.arange(grid_size):
+                lp = (lc-grid_size//2) * lm_step
+                for mc in numpy.arange(grid_size):
+                    mp = (mc-grid_size//2) * lm_step
+                    if(lp**2 + mp**2 > 1):
+                        continue
+                    np = numpy.sqrt(1.0-lp**2-mp**2) - 1.0
+                    b1 = find_beam_at_point(beams[i,:],[lp,mp,0])
+                    b2 = find_beam_at_point(beams[j,:],[lp,mp,0])
+                    bap = b1 * numpy.conj(b2) # Each antenna has same beam so square
+                
+                    phase_factor = numpy.exp(-2j * numpy.pi * (ud*lp + vd*mp + wd*np))
+                    point_amp = sky[mc,lc]
+                    vis_matrix[i,j] += point_amp * bap * phase_factor
+                
+    return vis_matrix/(grid_size**2)
 
     
 #### Closure Phases #### 
@@ -469,3 +583,147 @@ def simulate_electric_fields_different_beams(locations_uvw,points,beam_matrices)
         #elec_ss[i] += antenna_noise
     return elec_ss
 
+###### Airy Disc Beam Functions ##########
+
+def create_circular_mask(h, w, center=None, radius=None):
+
+    if center is None: # use the middle of the image
+        center = [int(w/2), int(h/2)]
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = numpy.ogrid[:h, :w]
+    dist_from_center = numpy.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = dist_from_center <= radius
+    return mask
+
+
+def generate_airy_power_beams_pointing_errors(locs_uvw,sf,point_error = None,bw_error = None):
+    
+    # Locs = antenna locs in u/v/w (wavelengths)
+    # Mean = Mean loc of antenna beam. Zero will suffice if centred on zenith in direction cosine co-ordinates
+    # lb = Lower bound of sigmas for real part of beam
+    # ub = Upper bound ""
+    # lbp = Lower bound of sigmas for imaginary party of beam
+    # ubp = Upper bound "" 
+    
+    beams_matrix = numpy.zeros(shape=(locs_uvw.shape[0],2000,2000),dtype=numpy.float64)
+   
+    print(beams_matrix.shape)
+    for i in numpy.arange(locs_uvw.shape[0]):
+        
+        
+        # Generates airy disc function in sky cosine co-ordinates
+        
+        if(point_error is None):
+            sigma_l = 0
+            sigma_m = 0
+        else:
+            sigma_l = numpy.random.normal(0,point_error)
+            sigma_m = numpy.random.normal(0,point_error)
+        
+        ls = numpy.linspace(-1+sigma_l,1+sigma_l,2000)
+        ms = numpy.linspace(-1+sigma_m,1+sigma_m,2000)
+        
+        #ls = numpy.linspace(-1,1,2000)
+        #ms = numpy.linspace(-1,1,2000)
+    
+        LS,MS = numpy.meshgrid(ls,ms)
+        
+        # Add 1 degree of width modulation to the beam
+        if(bw_error is None):
+            sigma_bw = 1
+        else:
+            sigma_bw = numpy.random.normal(1.0,bw_error)
+        airy_disc = (2*jv(1,sf*sigma_bw*(LS**2 + MS **2))/(sigma_bw*sf*(LS**2 + MS**2)))**2
+        #airy_disc = (2*jv(1,sf*(LS**2 + MS **2))/(sf*(LS**2 + MS**2)))**2
+        
+        ipu = 1000
+        ipus = 1.5*(ipu/sf)
+        
+        # Find Nulls and scale inbetween
+        nulls = numpy.asarray([3.8317,7.0156,10.1735,13.3237,16.4706])
+        nulls_s = nulls * ipus
+        nulls_i = nulls_s.astype(int)
+        print(nulls_i)
+        
+        # Scale outside primary beam
+        mask = create_circular_mask(2000,2000,radius=nulls_i[0])
+        #print(mask.shape)
+        #print(airy_disc.shape)
+        airy_disc[mask] *= 10
+        
+        # Normalise to unit amplitude
+        beam_peak = numpy.max(numpy.abs(airy_disc))
+        beams_matrix[i,:] = airy_disc / beam_peak
+        
+        
+        
+        
+    mean_beam = numpy.mean(beams_matrix,axis=0) # Beam error
+    beam_error = numpy.square(numpy.abs(mean_beam - beams_matrix)/numpy.abs(mean_beam))
+    av_beam_error = numpy.mean(beam_error)
+    print(av_beam_error)
+    
+
+    
+    return ls,ms,beams_matrix,av_beam_error
+
+def generate_airy_power_beams_with_sidelobes(no_of_beams,sf,gain_sigma):
+    
+    # Locs = antenna locs in u/v/w (wavelengths)
+    # Mean = Mean loc of antenna beam. Zero will suffice if centred on zenith in direction cosine co-ordinates
+    # lb = Lower bound of sigmas for real part of beam
+    # ub = Upper bound ""
+    # lbp = Lower bound of sigmas for imaginary party of beam
+    # ubp = Upper bound "" 
+    
+    beams_matrix = numpy.zeros(shape=(locs_uvw.shape[0],2000,2000),dtype=numpy.float64)
+   
+    print(beams_matrix.shape)
+    for i in numpy.arange(locs_uvw.shape[0]):
+        
+        
+        # Generates airy disc function in sky cosine co-ordinates
+        sigma = numpy.random.normal(0,gain_sigma)
+        
+        ls = numpy.linspace(-1,1,2000)
+        ms = numpy.linspace(-1,1,2000)
+    
+        LS,MS = numpy.meshgrid(ls,ms)
+        
+        airy_disc = (2*jv(1,sf*(LS**2 + MS **2))/(sf*(LS**2 + MS**2)))**2
+        
+        lin_sigma = numpy.power(10,sigma/10)
+        ipu = 1000
+        ipus = 1.5*(ipu/sf)
+        
+        # Find Nulls and scale inbetween
+        nulls = numpy.asarray([3.8317,7.0156,10.1735,13.3237,16.4706])
+        nulls_s = nulls * ipus
+        nulls_i = nulls_s.astype(int)
+        print(nulls_i)
+        
+        # Scale outside primary beam
+        mask = create_circular_mask(2000,2000,radius=nulls_i[0])
+        #print(mask.shape)
+        #print(airy_disc.shape)
+        airy_disc[mask] *= 10
+        airy_disc[mask] *= lin_sigma
+        
+        # Normalise to unit amplitude
+        beam_peak = numpy.max(numpy.abs(airy_disc))
+        beams_matrix[i,:] = airy_disc / beam_peak
+        
+        
+        
+        
+    mean_beam = numpy.mean(beams_matrix,axis=0) # Beam error
+    beam_error = numpy.square(numpy.abs(mean_beam - beams_matrix)/numpy.abs(mean_beam))
+    av_beam_error = numpy.mean(beam_error)
+    print(av_beam_error)
+    
+
+    
+    return ls,ms,beams_matrix,av_beam_error
